fe836c32e908b55f01025cd95b8c3a8442e3e11d
Xin Dong <113402835+TheDXport@users.noreply.github.com>
2022-10-04 12:02:50 -0400

    Add files via upload

---
+++ 01-favorite-movie/favoriteMovie.java
@@ -0,0 +1,10 @@
+class favoriteMovie {
+  public static void main(String[] args) {
+    System.out.print("The Last Samurai, a 2003 film, starring Tom Cruise is a movie that honors the loyalty and duties a samurai must possess. ");
+    System.out.print("Nathan, a former US Army captain is assigned to train newly recruited Japanese troops. ");
+    System.out.print("While Nathan is in a training session, he is informed of an attack by the Samurais in which he leads a handful of soldiers to fight. ");
+      System.out.print("Towards the end of the battle, as the last remaining soldier, he is outnumbered but decides to ferociusly fight for his life. ");
+      System.out.print("The Samurai leader of the group admires his tenacity and will to fight so he takes Nathan in as his own people ");
+      System.out.print("In the future, another confrontation commences as Nathan (leading the Samurais) is fighting against the very own Japanese troops he once trained.");
+  }
+}
